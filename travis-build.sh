883de0ee27ac3b6795362c96d04f8e6e83a455fb
Hirofumi Wakasugi <baenej@gmail.com>
2015-11-17 15:32:55 +0900

    リビルド自動化のためのTravis CIの設定をja用に変更

--- travis-build.sh
+++ travis-build.sh
@@ -6,10 +6,10 @@ rev=$(git rev-parse --short HEAD)
 
 bundle exec rake docs docs:tidy
 
-git config --global user.email "bcardiff@manas.com.ar"
-git config --global user.name "Travis on behalf Brian J. Cardiff"
+git config --global user.email "baenej@gmail.com"
+git config --global user.name "Travis on behalf Hirofumi Wakasugi"
 
-git remote add upstream "https://$GH_TOKEN@github.com/manastech/crystal"
+git remote add upstream "https://$GH_TOKEN@github.com/crystal-jp/ja.crystal-lang.org"
 git fetch upstream
 git reset upstream/gh-pages
 
